dfc13618c8bd3a1052e50af73af4e0a765b4b408
Simon507 <Evgeny.klysak@gmail.com>
2023-02-14 17:22:17 +0200

    разметка первой таски

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,14 @@
+import data from './json/user.json';
+import { Profile } from './userProfile/Profile';
+
 export const App = () => {
-  return <>HELLO WORLD</>;
+  return (
+    <Profile
+      username={data.username}
+      tag={data.tag}
+      location={data.location}
+      avatar={data.avatar}
+      stats={data.stats}
+    />
+  );
 };
